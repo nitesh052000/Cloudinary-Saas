17a1ef05ea26c68ae893a4ed1bdd73230f21e502
niteshkhandelewal <130399175+niteshkhandelewal@users.noreply.github.com>
2025-02-20 13:32:44 +0530

    edit route path

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@prisma/client";
-import { NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 
-// Ensure Prisma does not create multiple instances in serverless environments
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-// Force this route to be dynamic (prevents static export issues)
+// Force dynamic API route (prevents static export issues)
 export const dynamic = "force-dynamic";
 
 export async function GET() {
@@ -15,8 +14,6 @@ export async function GET() {
         const videos = await prisma.video.findMany({
             orderBy: { createdAt: "desc" },
         });
-
-        console.log("videos", videos);
 
         return NextResponse.json(videos);
     } catch (error) {
